3926b6ed3eb26826d426090ccd76d662848b8c4e
Nikos Kapraras <nikos@kapraran.dev>
2023-11-12 22:19:35 +0200

    add throttle util

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -15,3 +15,27 @@ export function formatCurrency(amount: number, currency?: string): string {
       .join("") + ` ${currency || ""}`
   ).trim();
 }
+
+/**
+ * Throttles a given function, ensuring it's not called more frequently than the specified delay.
+ *
+ * @param cb - The function to be throttled.
+ * @param delay - The minimum time (in milliseconds) that must elapse between two consecutive invocations of the throttled function.
+ * @returns A throttled version of the input function.
+ */
+export function throttle(cb: Function, delay: number) {
+  // Variable to track the time until the next allowed invocation
+  let wait = -1;
+
+  // Returning a throttled version of the input function
+  return (...args: any) => {
+    // If the wait time is still active, skip the invocation
+    if (wait >= 0) return;
+
+    // Invoke the original function with the provided arguments
+    cb(...args);
+
+    // Set a timeout to reset the wait time after the specified delay
+    wait = setTimeout(() => (wait = -1), delay);
+  };
+}
